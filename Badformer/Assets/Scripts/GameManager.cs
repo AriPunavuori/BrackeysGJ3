5ec5e68253730bed53202efdc5a98b59b9362cda
Ari Punavuori <ari.punavuori@gmail.com>
2019-08-27 14:39:33 +0300

    GameManager, PlayerController

---
+++ Badformer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public int lives;
+    public int livesStart;
+    public int health;
+    public int healthStart;
+
+    bool gameStarted;
+    bool gameHalted;
+    bool gameOver;
+    bool gameWon;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
---
+++ Badformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    float horizontalInput;
+    public float moveSpeed;
+    Rigidbody2D rb2d;
+
+    void Start() {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+    }
+
+    private void FixedUpdate() {
+        rb2d.MovePosition(new Vector2(transform.position.x + (horizontalInput * moveSpeed * Time.fixedDeltaTime),transform.position.y + 0));
+    }
+}
