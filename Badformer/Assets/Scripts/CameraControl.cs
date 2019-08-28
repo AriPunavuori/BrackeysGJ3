74aaa1735d34e74e61579da93fa3191649efa428
Ari Punavuori <ari.punavuori@gmail.com>
2019-08-28 21:05:44 +0300

    Camera pimping

--- Badformer/Assets/Scripts/CameraControl.cs
+++ Badformer/Assets/Scripts/CameraControl.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour {
 
     Transform target;
-
+    Vector2 offset = new Vector2(-3, 3);
     void Start() {
         target = GameObject.Find("Player").transform;
     }
 
-
     void Update() {
-        transform.position = new Vector3(target.position.x, target.position.y, -10);
+        transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, -10);
+        transform.rotation = Quaternion.LookRotation(target.position - transform.position, Vector3.up);
     }
 }
