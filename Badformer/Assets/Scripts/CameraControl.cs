b53ffed28993d39418d4cc8ffe9155a0e2b8508b
Ari Punavuori <ari.punavuori@gmail.com>
2019-08-28 21:25:19 +0300

    Camera test

--- Badformer/Assets/Scripts/CameraControl.cs
+++ Badformer/Assets/Scripts/CameraControl.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour {
 
     Transform target;
-    Vector2 offset = new Vector2(-3, 3);
+    Vector2 offset = new Vector2(-3f, 1.5f);
     void Start() {
         target = GameObject.Find("Player").transform;
     }
 
     void Update() {
-        transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, -10);
+        transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, - 7.5f);
         transform.rotation = Quaternion.LookRotation(target.position - transform.position, Vector3.up);
     }
 }
