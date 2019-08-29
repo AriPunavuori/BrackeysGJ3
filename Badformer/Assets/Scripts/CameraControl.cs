ca29b81cac4578d501e06a00c18386159c2b1159
Ari Punavuori <ari.punavuori@gmail.com>
2019-08-29 15:37:57 +0300

    Parrallaxing

--- Badformer/Assets/Scripts/CameraControl.cs
+++ Badformer/Assets/Scripts/CameraControl.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour {
 
     Transform target;
-    Vector2 offset = new Vector2(-3f, 1.5f);
+    public Vector2 offset = new Vector2(-3f, 1.5f);
     void Start() {
         target = GameObject.Find("Player").transform;
     }
 
     void Update() {
-        transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, - 7.5f);
-        transform.rotation = Quaternion.LookRotation(target.position - transform.position, Vector3.up);
+        transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, -7.5f);
+        //transform.rotation = Quaternion.LookRotation(target.position - transform.position, Vector3.up);
     }
 }
---
+++ Badformer/Assets/Scripts/Parallax.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+    public float parallaxEffect;
+    float length;
+    float camPevPosX;
+    float camStartPosX;
+    GameObject cam;
+
+    void Start() {
+        cam = GameObject.Find("Main Camera");
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update() {
+        transform.position += new Vector3((cam.transform.position.x - camPevPosX) * parallaxEffect, transform.position.y, transform.position.z);
+        //transform.position = new Vector3(transform.position.x, cam.transform.position.y, transform.position.x);
+        camPevPosX = cam.transform.position.x;
+        if(cam.transform.position.x > transform.position.x + length * 1.5f)
+            transform.position += new Vector3(length * 3, 0, 0);
+        else if(cam.transform.position.x < transform.position.x - length * 1.5f)
+            transform.position -= new Vector3(length * 3, 0, 0);
+    }
+}
