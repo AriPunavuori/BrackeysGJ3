e515f4362a3cbb227059808b79e31b02f22fc1c8
Ari Punavuori <ari.punavuori@gmail.com>
2019-08-30 12:46:21 +0300

    New parallax (Split to 2 different scripts)

---
+++ Badformer/Assets/Scripts/ParallaxMoving.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxMoving : MonoBehaviour {
+
+    float length;
+    GameObject cam;
+
+    void Start() {
+        cam = GameObject.Find("Main Camera");
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update() {
+        if(cam.transform.position.x > transform.position.x + length * 1.5f)
+            transform.position += new Vector3(length * 3, 0, 0);
+        else if(cam.transform.position.x < transform.position.x - length * 1.5f)
+            transform.position -= new Vector3(length * 3, 0, 0);
+    }
+}
