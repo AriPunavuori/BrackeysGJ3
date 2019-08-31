675c2c88dff82bb500ee316ffedae8c4cecaf570
Ari Punavuori <ari.punavuori@gmail.com>
2019-08-31 11:03:35 +0300

    Camera y-axis lerp

--- Badformer/Assets/Scripts/CameraControl.cs
+++ Badformer/Assets/Scripts/CameraControl.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour {
 
     Transform target;
-    public Vector2 offset = new Vector2(-3f, 1.5f);
+    float speed = .75f;
+    //public Vector2 offset = new Vector2(-3f, 1.5f);
     void Start() {
         target = GameObject.Find("Player").transform;
     }
 
     void Update() {
-        transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, -7.5f);
+        //transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, -7.5f);
         //transform.rotation = Quaternion.LookRotation(target.position - transform.position, Vector3.up);
+
+        float interpolation = speed * Time.deltaTime;
+
+        Vector3 position = transform.position;
+        position.y = Mathf.Lerp(transform.position.y, target.transform.position.y, interpolation);
+        position.x = target.transform.position.x;
+
+        transform.position = position;
+
     }
 }
--- Badformer/Assets/Scripts/ParallaxShifting.cs
+++ Badformer/Assets/Scripts/ParallaxShifting.cs
@@ -7,15 +7,15 @@ public class ParallaxShifting : MonoBehaviour {
     public float verticalParallax;
     float camPevPosX;
     float camStartPosX;
-    GameObject player;
+    GameObject cam;
 
     void Start() {
-        player = GameObject.Find("Player");
+        cam = GameObject.Find("Main Camera");
     }
 
     void Update() {
-        transform.position += new Vector3((player.transform.position.x - camPevPosX) * horizontalParallax, transform.position.y, transform.position.z);
-        transform.position = new Vector3(transform.position.x, player.transform.position.y * verticalParallax, transform.position.x);
-        camPevPosX = player.transform.position.x;
+        transform.position += new Vector3((cam.transform.position.x - camPevPosX) * horizontalParallax, transform.position.y, transform.position.z);
+        transform.position = new Vector3(transform.position.x, cam.transform.position.y * verticalParallax, transform.position.x);
+        camPevPosX = cam.transform.position.x;
     }
 }
