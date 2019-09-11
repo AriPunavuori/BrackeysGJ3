abd707fe9481fcad5ffd69acd31c6bf918e3d5e7
Ari Punavuori <ari.punavuori@gmail.com>
2019-09-11 19:57:59 +0300

    PatrolCar

---
+++ Badformer/Assets/Scenes/EnemyPatrolCar.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrolCar : MonoBehaviour {
+    Rigidbody2D rb;
+    public Vector2 patrolPoint;
+    public Vector2 patrolAreaSizeX;
+    public Vector2 patrolAreaSizeY;
+    GameObject drone;
+    Vector2 startPos;
+    public float speed;
+    float waitTimer;
+    float waitTime = 2f;
+    bool droneLaunched;
+
+    void Start() {
+        drone = transform.Find("EnemyDrone").gameObject;
+        drone.gameObject.SetActive(false);
+        rb = GetComponent<Rigidbody2D>();
+        startPos = transform.position;
+        RandomizeNextPoint();
+    }
+
+    void Update() {
+        
+    }
+
+    private void FixedUpdate() {
+        var movementDelta = (patrolPoint - (Vector2)transform.position) * Time.deltaTime * speed;
+        print("MovementDelta" + movementDelta);
+        print("Position:" + ((Vector2)transform.position));
+        rb.MovePosition((Vector2)transform.position + movementDelta);
+        if(Vector2.Distance(transform.position, patrolPoint) < 0.1f) {
+            rb.MovePosition(patrolPoint);
+            RandomizeNextPoint();
+        }
+    }
+
+    void RandomizeNextPoint() {
+        patrolPoint.x = startPos.x + Random.Range(patrolAreaSizeX.x, patrolAreaSizeX.y);
+        if(patrolPoint.x < transform.position.x)
+            transform.localScale = new Vector3(1, 1, 1);
+        else
+            transform.localScale = new Vector3(-1, 1, 1);
+        patrolPoint.y = startPos.y + Random.Range(patrolAreaSizeY.x, patrolAreaSizeY.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.tag == "Player") {
+            if(!droneLaunched) {
+                drone.gameObject.SetActive(true);
+                drone.transform.SetParent(null);
+                droneLaunched = true;
+            }
+        }
+    }
+
+    public void DroneLost() {
+        droneLaunched = false;
+        drone.transform.SetParent(this.transform);
+        drone.gameObject.SetActive(false);
+        
+    }
+}
--- Badformer/Assets/Scripts/EnemyDrone.cs
+++ Badformer/Assets/Scripts/EnemyDrone.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class EnemyDrone : MonoBehaviour {
     public GameObject ammoPrefab;
+    public GameObject dummyTarget;
 
     float FiringDist = 15f;
     int ammoBurstAmount = 5;
@@ -12,14 +14,26 @@ public class EnemyDrone : MonoBehaviour {
     float firingInterval = 1.5f;
     float shotTimer;
     float firingTimer;
+    float getLostTimer;
+    float getLostTime = 5f;
+    AIDestinationSetter ds;
+    EnemyPatrolCar pc;
 
     public bool inSight;
     public bool startFiring;
-
+    
     GameObject player;
 
+    private void Awake() {
+        pc = transform.parent.GetComponent<EnemyPatrolCar>();
+    }
+
     private void Start() {
+
+        getLostTimer = getLostTime;
         player = GameObject.FindGameObjectWithTag("Player");
+        ds = GetComponent<AIDestinationSetter>();
+        ds.target = player.transform;
     }
 
     void Update() {
@@ -40,8 +54,21 @@ public class EnemyDrone : MonoBehaviour {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, player.transform.position - transform.position);
         if(hit.collider.gameObject == player && hit.distance < FiringDist) {
             inSight = true;
+            getLostTimer = getLostTime;
+            ds.target = player.transform;
         } else {
             inSight = false;
+        }
+        if(!inSight) {
+            getLostTimer -= Time.fixedDeltaTime;
+        }
+        if (getLostTimer < 0) {
+            ds.target = pc.transform;
+            if(Vector2.Distance(transform.position, pc.transform.position) < 1.6f) {
+                transform.position = pc.transform.position;
+                getLostTimer = getLostTime;
+                pc.DroneLost();
+            }
         }
     }
 
@@ -58,6 +85,9 @@ public class EnemyDrone : MonoBehaviour {
             ammoAmount = 0;
             startFiring = false;
         }
+    }
 
+    public void SetPatrolCar(EnemyPatrolCar e) {
+        pc = e;
     }
 }
